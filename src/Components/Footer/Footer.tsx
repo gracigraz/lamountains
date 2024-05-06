6a4c0269341b6c3ad6943d17b836f7f223ad0919
Graciela Manzanares <38384752+gracigraz@users.noreply.github.com>
2024-05-06 12:40:40 -0400

    updated footer styling to match mockup

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,13 +1,15 @@
 
 import "./Footer.scss";
-import logo from "../../assets/images/BrainFlix-logo.svg";
+import logo from "../../assets/images/company-logo.svg";
 import { NavLink } from "react-router-dom";
 
 const Footer: React.FC = () => {
     return (
     <footer className="footer">
         <div className="footer__container">
-            <img src="" alt="" className="footer__logo"/>
+            <div className="footer__logo-wrapper">
+                <img src={logo} alt="LA Mountain Company Logo" className="footer__logo"/>
+            </div>
 			<span className="footer__company-name">LOSANGELES MOUNTAINS</span>
         </div>
 			<p className="footer__copyright">COPYRIGHT 2016. ALL RIGHTS RESERVED</p>
